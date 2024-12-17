61cbd723e363fc4e750ed0b73e7a56bc900978c8
Sergi Ortiz Gomez <drualcman@gmail.com>
2024-12-17 09:20:58 +0800

    Document swagger responses

--- src/AuthifyPass.API/Endpoints/ClientEndPoints.cs
+++ src/AuthifyPass.API/Endpoints/ClientEndPoints.cs
@@ -7,10 +7,15 @@ public static class ClientEndPoints
         RouteGroupBuilder mainGroup = app.MapGroup("client").WithTags("Client");
 
         mainGroup.MapPost("", async (RegisterClientDto data,
-                    IRegisterClientController controller) => Results.Ok(await controller.CreateClientAsync(data)))
-            .Produces<RegisterClientResponseDto>(StatusCodes.Status200OK);
-        mainGroup.MapDelete("{id}", async (string id, HttpContext context, IDeleteClientController controller) =>
-        await controller.DeleteClient(id, HeaderHelper.GetSharedKeyHeader(context)));
+                    IRegisterClientController controller) =>
+                Results.Ok(await controller.CreateClientAsync(data)))
+            .Produces<RegisterClientResponseDto>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status500InternalServerError);
+        mainGroup.MapDelete("{id}", async (string id, HttpContext context,
+                    IDeleteClientController controller) =>
+                await controller.DeleteClient(id, HeaderHelper.GetSharedKeyHeader(context)))
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound);
 
         return app;
     }
--- src/AuthifyPass.API/Endpoints/UserEndPoints.cs
+++ src/AuthifyPass.API/Endpoints/UserEndPoints.cs
@@ -9,13 +9,20 @@ public static class UserEndPoints
         RouteGroupBuilder mainGroup = app.MapGroup("user").WithTags("User");
 
         mainGroup.MapPost("", async (RegisterUserClientDto data, HttpContext context,
-                        IRegisterUserInputPort input) => Results.Ok(await input.RegisterUserAsync(data, HeaderHelper.GetSharedKeyHeader(context))))
-            .Produces<RegisterUserClientResponseDto>(StatusCodes.Status200OK);
+                        IRegisterUserInputPort input) =>
+                Results.Ok(await input.RegisterUserAsync(data, HeaderHelper.GetSharedKeyHeader(context))))
+            .Produces<RegisterUserClientResponseDto>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status500InternalServerError);
         mainGroup.MapPost("/validate-code", async (ValidateUserCodeDto data, HttpContext context,
-                        IValidateUserCodeInputPort input) => Results.Ok(await input.ValidateUserCode(data, HeaderHelper.GetSharedKeyHeader(context))))
-            .Produces<bool>(StatusCodes.Status200OK);
-        mainGroup.MapDelete("{id}", async (string id, HttpContext context, IDeleteUserController controller) =>
-        await controller.DeleteUser(id, HeaderHelper.GetSharedKeyHeader(context)));
+                        IValidateUserCodeInputPort input) =>
+                Results.Ok(await input.ValidateUserCode(data, HeaderHelper.GetSharedKeyHeader(context))))
+            .Produces<bool>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status500InternalServerError);
+        mainGroup.MapDelete("{id}", async (string id, HttpContext context,
+                        IDeleteUserController controller) =>
+                await controller.DeleteUser(id, HeaderHelper.GetSharedKeyHeader(context)))
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound);
         return app;
     }
 }
