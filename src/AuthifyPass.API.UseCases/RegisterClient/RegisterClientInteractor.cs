036ac51d2af86ad023ebf5ce6de3464415d37591
Sergi Ortiz Gomez <drualcman@gmail.com>
2024-12-26 08:32:23 +0800

    Refactor to return a exception if try to register same email address for a new client. Only one email address are valid to get a client id.

--- src/AuthifyPass.API.UseCases/RegisterClient/RegisterClientInteractor.cs
+++ src/AuthifyPass.API.UseCases/RegisterClient/RegisterClientInteractor.cs
@@ -2,22 +2,39 @@
 internal class RegisterClientInteractor(IClientRepository repository,
     IModelValidatorHub<RegisterClientDto> validator,
     IRegisterClientOutputPort output,
+    IStringCulture<DatabaseErrors> localizer,
     IIdentifierGenerator identifierGenerator) : IRegisterClientInputPort
 {
     public async Task Handle(RegisterClientDto register)
     {
         await GuardModel.AgainstNotValid(validator, register);
-
+        await ThrowIfEmailExists(register.Email);
         string clientId = identifierGenerator.GenerateClientId();
         string sharedSecret = identifierGenerator.GenerateSharedSecret();
-        AddClientDto client = new(
-            clientId: clientId,
-            name: register.Name,
-            email: register.Email,
-            password: identifierGenerator.ComputeSha256Hash(register.Password),
-            sharedSecret: sharedSecret
-            );
+        AddClientDto client = CreateClient(register, clientId, sharedSecret);
         await repository.AddClientAsync(client);
         await output.Handle(register.Name, clientId, sharedSecret);
+    }
+
+    private async Task ThrowIfEmailExists(string email)
+    {
+        Client existingClient = await repository.GetClientByEmailAsync(email);
+        if (existingClient is not null)
+            throw new ValidationException(new List<ValidationError>()
+        {
+            new ValidationError(nameof(RegisterClientDto.Email),
+                                string.Format(localizer.GetString(nameof(DatabaseErrors.DuplicateEmailErrorTemplate)), email))
+        });
+    }
+
+    private AddClientDto CreateClient(RegisterClientDto register, string clientId, string sharedSecret)
+    {
+        return new(
+                    clientId: clientId,
+                    name: register.Name,
+                    email: register.Email,
+                    password: identifierGenerator.ComputeSha256Hash(register.Password),
+                    sharedSecret: sharedSecret
+                    );
     }
 }
